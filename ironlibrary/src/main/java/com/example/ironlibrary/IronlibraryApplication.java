16bba2b311ac4d2b58125a74e226f8b3719ac0b9
jtiradohernandez <javiertirhe@gmail.com>
2024-03-16 09:51:06 +0100

    Base of the program

---
+++ ironlibrary/src/main/java/com/example/ironlibrary/IronlibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.ironlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronlibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronlibraryApplication.class, args);
+	}
+
+}
