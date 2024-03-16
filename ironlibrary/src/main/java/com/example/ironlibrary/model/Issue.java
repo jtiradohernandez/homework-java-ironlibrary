66887f26e57b919a7e3da0b9a1e86ee03533022a
Darren <111098562+DJH23@users.noreply.github.com>
2024-03-16 12:35:26 +0100

    add InputValidator implementation to Issue model

--- ironlibrary/src/main/java/com/example/ironlibrary/model/Issue.java
+++ ironlibrary/src/main/java/com/example/ironlibrary/model/Issue.java
@@ -7,12 +7,19 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
-public class Issue {
+public class Issue implements InputValidator{
     private int issueId;
     private String issueDate;
     private String returnDate;
     // To be used later.
     //private Student issueStudent;
     //private Book issueBook;
+
+    @Override
+    public boolean input_validation() {
+        // Implement validation logic here
+        // Return true if validation passes, false otherwise
+        return true;
+    }
 
 }
