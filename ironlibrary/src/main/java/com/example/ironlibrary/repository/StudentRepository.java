81992940e1e72689e505cd767c04a26ece6f4411
Katia <katia.obr@gmail.com>
2024-03-16 14:06:05 +0100

    subit tests y student repository

--- ironlibrary/src/main/java/com/example/ironlibrary/repository/StudentRepository.java
+++ ironlibrary/src/main/java/com/example/ironlibrary/repository/StudentRepository.java
@@ -1,2 +1,11 @@
-package com.example.ironlibrary.repository;public interface StudentRepository {
+package com.example.ironlibrary.repository;
+
+import com.example.ironlibrary.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student, String> {
+
+    //Optional<Student> findByName(String name);
 }
